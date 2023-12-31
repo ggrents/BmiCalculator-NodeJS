e00e3775d5baec94db01a341fb44ec5a42ebdef4
ggrents <chef_grents@mail.ru>
2023-12-31 13:02:23 +0600

    finish the project

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
+// calculator.js
+
 const express = require('express');
-
-// Create an express application
+const bodyParser = require('body-parser'); 
+const validator = require('validator');
 const app = express();
+const port = 3000;
+app.use(express.static('static'));
 
-// Create a root route with a GET method
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.get('/', (req, res) => {
-  // Send the words "Hello World" as the response
-  res.send('Hello World');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-// Spin up the server on port 3000
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.get('/bmi', (req, res) => {
+    res.sendFile(__dirname + '/views/bmi.html');
+});
+
+app.post('/bmi', (req, res) => {
+    const height = parseFloat(req.body.height);
+    const weight = parseFloat(req.body.weight);
+    const age = parseInt(req.body.age);
+    const gender = req.body.gender;
+    const unit = req.body.unit;
+
+    if (!validator.isFloat(String(height)) || !validator.isFloat(String(weight)) || !validator.isInt(String(age))) {
+        return res.status(400).send('Invalid input. Please enter numeric values for height, weight, and age.');
+    }
+
+    const heightInMeters = (unit === 'metric') ? height : height * 0.0254;
+    const weightInKg = (unit === 'metric') ? weight : weight * 0.453592;
+
+
+    const bmi = (weightInKg / (heightInMeters * heightInMeters)).toFixed(2);
+
+
+    let interpretation = '';
+    if (bmi < 18.5) {
+        interpretation = 'Underweight';
+    } else if (bmi < 25) {
+        interpretation = 'Normal weight';
+    } else if (bmi < 30) {
+        interpretation = 'Overweight';
+    } else {
+        interpretation = 'Obese';
+    }
+
+
+    res.send(`<h2>BMI Result: ${bmi}</h2><p>Interpretation: ${interpretation}</p>`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
